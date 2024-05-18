743695db66422388505709cbf9ace6ce8147a619
Vish katyan <82721870+vishkatyan@users.noreply.github.com>
2024-05-18 12:14:08 +0530

    Solve Me First

---
+++ solveMeFirst.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+public class MyClass {
+    public static void main(String args[]) {
+      
+      
+      Scanner in = new Scanner(System.in);
+      int a ;
+      int b ; 
+      int c ;
+      a = in.nextInt();
+      b = in.nextInt();
+      c = a + b ; 
+      
+      System.out.println(c);
+    }
+}
